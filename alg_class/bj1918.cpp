546db437e5d2ba99c0ce915536b332f1fc65354d
seungmin001 <minnie9808@gmail.com>
2021-01-19 18:43:03 +0900

    bj1918 postfix notation temp store

---
+++ alg_class/bj1918.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//중위 -> 후위 표기식 postfix notation
+//실패했던 이유 : 처음에는 괄호가 없는 경우를 생각하지 않았다.
+//나중에 다시 도전
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string str;
+    cin >> str;
+
+    
+ 
+    return 0;
+}
